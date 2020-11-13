633624354a41555a8adc82e52452ecd79064c1a3
bartekwisnia <bartekwisnia@op.pl>
2020-11-13 22:13:20 +0100

    aaa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import App from "./components/App";
 import './static/bulma.css';
 import './static/index.css';
